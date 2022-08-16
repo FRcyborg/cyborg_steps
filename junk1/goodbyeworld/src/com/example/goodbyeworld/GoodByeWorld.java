c7a6210781593ad3bbd6e2cd38442339e4224eb5
oldmanj <john.farrell@colostate.edu>
2022-08-16 00:23:04 -0600

    made example goodbyeworld program java

---
+++ junk1/goodbyeworld/src/com/example/goodbyeworld/GoodByeWorld.java
@@ -0,0 +1,12 @@
+package com.example.goodbyeworld;
+
+public class GoodByeWorld {
+    public static void main(String[] args) {
+        System.out.println("\n***********************************************************\n");
+        System.out.println("Good Bye, Cruel World!");
+        System.out.println("I'm leaving you today.");
+        System.out.println("goodbye, goodbye, goodbye...");
+        System.out.println("goodbye all you people.  \nThere's nothing you can say\nto make me change my mind. \ngoodbye");
+        System.out.println("\n***********************************************************\n");
+    }
+}
