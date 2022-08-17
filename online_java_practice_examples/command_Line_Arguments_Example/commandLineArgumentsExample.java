7a8dabb970c1983b9d65b8fd2ddc18c2bf79f2a2
oldmanj <john.farrell@colostate.edu>
2022-08-17 00:08:23 -0600

    Big clean mostly and a few practice files

--- online_java_practice_examples/command_Line_Arguments_Example/commandLineArgumentsExample.java
+++ online_java_practice_examples/command_Line_Arguments_Example/commandLineArgumentsExample.java
@@ -1,5 +1,16 @@
-class jDare {
+/*
+    This program shows how to deal with the args in "main(String[] args)"
 
+    To use this you need to include command line arguments at run time:
+
+    $ java commandLineArgumentsExample string1 string2 <enter>
+
+    jlf  -  20220816
+*/
+
+
+
+class commandLineArgumentsExample {
 
       public static void makeLine() 
       {
@@ -7,33 +18,18 @@ class jDare {
       }
 
 
-      public static int someThing(int i) {
-      
-            for ( int x=0; x<10; x++) 
-            {
-                  System.out.println("x is now => " + x);
-            }
-            
-            return i * i;
-      }     
-
       // => String[] <= is an array of strings
       public static void main(String[] args) {
+
+            makeLine();
 
             for ( String s : args ) 
             {
                   System.out.println("string is => " + s);
             }     
 
-
-
-      
             makeLine();
              
-            System.out.println("did something happen?");
-
-            makeLine();
-             
-}
+        }
 
 }
