9d19db968d7b37b2f01e4c159923f5592a786d65
oldmanj <john.farrell@colostate.edu>
2022-08-16 20:38:19 -0600

    more java/intellij practice

---
+++ carnegie_mellon_java1/PrimitiveVariables.java
@@ -0,0 +1,39 @@
+// Java Program to Demonstrate Char Primitive Data Type
+
+// Class
+class PrimitiveVariables {
+
+    // Main driver method
+    public static void main(String args[]) {
+
+        // Creating and initializing custom character
+        char a = 'G';
+
+        // Integer data type is generally
+        // used for numeric values
+        int i = 89;
+
+        // use byte and short 
+        // if memory is a constraint
+        byte b = 4;
+
+        short s = 56;
+
+        double d = 4.355453532;
+
+        float f = 4.73334343f;
+
+        long l = 12121;
+
+        System.out.println("char: " + a);
+        System.out.println("integer: " + i);
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("long: " + l);
+
+    }
+}
+
+
---
+++ junk1/junk3/some.java
@@ -0,0 +1,13 @@
+public class some {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("\n***************************************************\n");
+        System.out.println("what goes up");
+        System.out.println("\n***************************************************\n");
+
+    }
+
+
+}
