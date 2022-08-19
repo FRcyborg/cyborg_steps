3ce28df9ede27c0d03a9f724473eec83270bae2f
oldmanj <john.farrell@colostate.edu>
2022-08-19 01:17:39 -0600

    just random practice

---
+++ junk1/junk3/randomCheck.java
@@ -0,0 +1,31 @@
+public class randomCheck {
+
+    public static void main(String[] args) {
+   
+        makeLine();
+
+        System.out.println("did something happen?");
+
+        addNumbers();
+
+        makeLine();
+
+
+    } // end of main()  
+
+    public static void makeLine()
+    {
+        System.out.println("\n*************************************************************\n");
+    }
+
+    static int addNumbers() 
+    {
+        System.out.println("We'z in addNumbers");
+        return 5;
+    }
+
+
+
+}  // we better go check that
+
+
