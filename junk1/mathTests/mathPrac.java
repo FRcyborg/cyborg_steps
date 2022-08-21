ea10aa340bb1f6a8c0010a614e8e78ed0e8cb699
oldmanj <john.farrell@colostate.edu>
2022-08-20 19:57:07 -0600

    starting in on java for school

---
+++ junk1/mathTests/mathPrac.java
@@ -0,0 +1,30 @@
+/*
+   this program is for testing out math handling in java.  See
+   what the types do and how they behave.
+
+   20220820  -  JLF
+*/
+
+
+class mathPrac {
+
+
+    static public void main(String[] args) {
+
+        System.out.println("here's something!");
+
+        System.out.println("this is => " + divideIntegers());
+    
+
+    }  // end of main
+
+    static public int divideIntegers() {
+
+        int A = 2;
+        int B = 5;
+
+        return B / A;
+    } 
+
+
+}
