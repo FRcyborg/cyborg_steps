d9b7c66ae4c9b04c2fa5ebd74c128bf832460394
JLF <jlf.inbox@gmail.com>
2022-08-25 23:04:13 -0600

    tuple practice

---
+++ junk1/tuple_python.py
@@ -0,0 +1,33 @@
+def makeLine():
+      print("\n-------------------------------------------------------------\n")
+
+
+def main():
+      makeLine()
+
+      A = {"0","1"}
+      B = {"2","3"}
+      C = {"4", "5", "6"}
+
+
+      print(A)
+      print(B)
+      print(C)
+
+      makeLine()
+
+      for x in A:
+            print(x)
+
+      for x in B:
+            print(x)
+
+      for x in C:
+            print(x)
+
+
+
+      makeLine()
+
+
+main()
