501e1aaad0543df5e46df6ff4d1f21592638a4fc
oldmanj <john.farrell@colostate.edu>
2022-08-26 23:39:09 -0600

    program 1 CS-165 mostly

---
+++ CS-165 Fall 2022/1.24 Lab 0:Using Objects/com.cs165.1.24lab/src/NotGonnaUseThis.java
@@ -0,0 +1,18 @@
+public class NotGonnaUseThis {
+
+    private static void firstPrint() {
+            System.out.println("Not gonna use this class!");
+    }
+
+    private static void makeLine()
+    {
+        System.out.println("\n---------------------------------------------------\n");
+    }
+
+    public static void main(String[] args) {
+        makeLine();
+    //    firstPrint();
+        makeLine();
+    } // end of main
+
+} // end of class
