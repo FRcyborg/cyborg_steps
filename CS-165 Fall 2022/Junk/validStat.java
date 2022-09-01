39ea2b0f6582db08b6c14450d46ae58d449a93ae
oldmanj <john.farrell@colostate.edu>
2022-08-31 23:01:37 -0600

    stupid homework

---
+++ CS-165 Fall 2022/Junk/validStat.java
@@ -0,0 +1,20 @@
+public class validStat {
+
+    public static void main(String[] args) {
+        
+//        System.out.println("(true && (false || true) || false) => " + (true && (false || true) || false));
+//        System.out.println("2 => " + "ABC".concat("DEF"));
+//        float x = 24.5 + "324";
+//        System.out.println("1 => " + x);
+        int y = (int)(24/3.0);
+        System.out.println("(int)(24/3.0) => " + y);
+//        int y1 = "33";
+//        System.out.println("2 => " + y1);
+        String s = 'a' + 'b' + 'c' + "d";
+        System.out.println("'a' + 'b' + 'c' + \"d\" => " + s);
+        double d1 = 20;
+        System.out.println("double d1 = 20; => " + d1);
+    }  // end of main()
+
+
+}  // end of class
