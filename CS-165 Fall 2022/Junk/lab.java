ae4c25f5e941700789036642c4d388c0292b352b
oldmanj <john.farrell.colostate.edu>
2022-09-02 13:06:54 -0600

    clean up on isle macbook

---
+++ CS-165 Fall 2022/Junk/lab.java
@@ -0,0 +1,14 @@
+public class Lab {
+
+      public static void main(String[] args) {
+            
+            System.out.println("\n--------------------\n");
+            System.out.println("");
+            
+            System.out.println("\n--------------------\n");
+
+      } // end of main
+
+
+
+}
---
+++ CS-165 Fall 2022/Junk/random1.java
@@ -0,0 +1,37 @@
+public class random1 {
+
+      public static void main(String[] args) {
+
+            System.out.println("3%9 => " + 3%9);
+            System.out.println("9%3 => " + 9%3);
+
+
+            System.out.println("(true && (false || true) || false) => " + (true && (false || true) || false));
+
+             int option=1;
+             switch(option){
+             case 1:
+              System.out.println("Selected option is 1!");
+             case 2:
+              System.out.println("Selected option is 2!");
+             case 3:
+              System.out.println("Selected option is 3!");
+             default:
+              System.out.println("Invalid option!"); 
+             }
+
+            String str4 = "abc" + 'x' + "def";
+            String str5 = "" + 24;
+            String str1 = "abc";
+            String str3 = new String("abc");
+//            String str2 = (String) 'a';
+              System.out.println(str4); 
+              System.out.println(str5); 
+              System.out.println(str1); 
+              System.out.println(str3); 
+                        
+
+      } // end of main
+
+
+} // end of class
