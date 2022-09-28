0ba4d030c0a7de8013d50adafc6b842094c1df08
oldmanj <john.farrell@colostate.edu>
2022-09-28 12:25:51 -0600

    stupid linked lists

--- CS-165 Fall 2022/LinkedListLab20220927/CS 165 Data Structures Nodes Linked/src/MyLinkedList.java
+++ CS-165 Fall 2022/LinkedListLab20220927/CS 165 Data Structures Nodes Linked/src/MyLinkedList.java
@@ -1,2 +1,97 @@
-public class MyLinkedList {
+/** Linked List Lab
+ * Made by Toby Patterson 5/29/2020
+ * For CS165 at CSU
+ */
+
+public class MyLinkedList implements MiniList<Integer>{
+    /* Private member variables that you need to declare:
+     ** The head pointer
+     ** The tail pointer
+     */
+    Node head;
+    Node tail;
+
+    public class Node {
+        // declare member variables (data and next)
+        int data;
+        Node next = null;
+
+        // finish these constructors
+        public Node(int data, Node next) {
+
+        }
+        public Node(int data) {} // HINT: use this() with next = null
+    }
+
+    // Initialize the linked list (set head and tail pointers)
+    public MyLinkedList() {}
+
+    @Override
+    public boolean add(Integer item) {
+
+        return false;
+    }
+
+    @Override
+    public void add(int index, Integer element) {
+
+    }
+
+    @Override
+    public Integer remove() {
+        return null;
+    }
+
+    @Override
+    public Integer remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Integer item) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(Integer item) {
+        return false;
+    }
+
+    @Override
+    public Integer get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Integer item) {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    // Uncomment when ready to test
+//    @Override
+//    public String toString() {
+//        String ret = "";
+//        Node curr = head;
+//        while (curr != null) {
+//            ret += curr.data + " ";
+//            curr = curr.next;
+//        }
+//        return ret;
+//    }
+
 }
---
+++ CS-165 Fall 2022/LinkedListLab20220927/scratchPad/linkListPractice.java
@@ -0,0 +1,191 @@
+public class linkListPractice {
+
+    Node head = null;
+    Node tail = null;
+
+    // -------------------------------------------------------------------------------------
+    public class Node {
+        
+        int data;
+        Node next = null;
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        } 
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        } 
+
+    } // end of class Node
+    // -------------------------------------------------------------------------------------
+
+    public boolean add(int item) {
+       Node newNode = new Node(item);
+       if ( head == null ) {
+           head = newNode;
+           tail = newNode;
+       } else {
+           tail.next = newNode;
+           tail = newNode;
+       } // end of if / else
+        
+       return false;
+
+    } // end of add
+
+    // -------------------------------------------------------------------------------------
+
+    public void add(int index, Integer element) {
+       Node newNode = new Node(element);
+       Node previousNode = head;
+       int whereToPutIt = index;
+
+       if ( size() <= whereToPutIt ) {
+          add(element);
+       } else if ( size() == 1 ) {
+               newNode.next = head.next;
+               head.next = newNode;
+           } // end of if
+       else {
+           for ( int i=0; i<index-1; i++ ) {
+              previousNode = previousNode.next;
+           }
+           newNode.next = previousNode.next;
+           previousNode.next = newNode;
+       } // end of else
+
+
+    } // end of add(1,2)
+
+    // -------------------------------------------------------------------------------------
+
+    public void showList() { 
+        Node current = head;
+        if ( head==null) {
+            System.out.println("List is empty");
+            return;
+        }
+        while(current != null) {
+            System.out.print(current.data+" ");
+            current = current.next;
+        }
+        System.out.println();
+    } // end of showList()
+    // -------------------------------------------------------------------------------------
+    public int size() {
+        int sizeOfThing=0;
+        Node tempNode = head;
+        
+        while ( tempNode != null ) {
+            sizeOfThing++;
+            tempNode = tempNode.next;
+        }
+
+        return sizeOfThing;
+    } // end of size()
+
+    // -------------------------------------------------------------------------------------
+    public int indexOf(Integer item) {
+        int sizeOfThing=0;
+        Node tempNode = head;
+        int indexOfHolder = 0;
+        
+        while ( tempNode != null ) {
+            if ( tempNode.data == item ) indexOfHolder = sizeOfThing;
+
+            sizeOfThing++;
+
+            tempNode = tempNode.next;
+        }
+
+
+        return indexOfHolder;
+        
+        
+    } // end of indexOf()
+    
+    // -------------------------------------------------------------------------------------
+
+    public Integer get(int index) {
+       int indexTracker = 0;
+       Node tempNode = head;
+       int dataHolder = 0;
+       
+       if ( head == null ) return null;
+       if ( size()-1 < index ) return null;
+       for ( int i=0; i<=index; i++ ) {
+           System.out.println("tempNode.data => " + tempNode.data);
+           dataHolder = tempNode.data;
+          tempNode = tempNode.next;
+       }
+       return dataHolder;
+         
+    }
+
+    // -------------------------------------------------------------------------------------
+
+        public boolean contains(Integer item) {
+            Node tempNode = head;
+            while ( tempNode != null ) {
+                if ( tempNode.data == item ) return true;
+                tempNode = tempNode.next;
+            }
+
+            return false;
+        }
+
+    // -------------------------------------------------------------------------------------
+
+        public void clear() {
+
+            head = null;
+            tail = null;
+
+
+        }  // end of clear()
+
+    // -------------------------------------------------------------------------------------
+
+    public static void makeLine() { System.out.print("\n-----------------------------------------\n"); }
+
+
+    // -------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------------
+
+    public static void main(String[] args) {
+        linkListPractice l = new linkListPractice();
+        makeLine();
+
+        l.showList();
+        l.add(4);
+        l.add(9);
+        l.add(5);
+        l.add(2);
+        l.showList();
+
+        System.out.println("checking add(2,7) => ");
+        l.add(2,7);
+        l.showList();
+
+        System.out.println("checking add(0,7) => ");
+        l.add(0,7);
+        l.showList();
+
+
+
+//        System.out.println("size of linkedList => " + l.size());
+//        System.out.println("indexOfHolder => " + l.indexOf(9));
+//        System.out.println("get function 2 returns => " + l.get(2));
+//        System.out.println("get function 1 returns => " + l.get(1));
+//        System.out.println("does it contain 5 => " + l.contains(5));
+//        System.out.println("does it contain 6 => " + l.contains(6));
+        l.clear();
+        l.showList();
+
+        makeLine();
+
+    } // end of main
+}
