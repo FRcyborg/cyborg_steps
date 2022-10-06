932c09bac77d5130b6ec23fcbcf920bf6f3afd4b
oldmanj <john.farrell@colostate.edu>
2022-10-06 15:09:36 -0600

    stuff

---
+++ CS-165 Fall 2022/test3study/src/Algorithm.java
@@ -0,0 +1,18 @@
+// Will the following class compile?  If not, why?
+
+public class Algorithm {
+    public static <T> T max ( T x, T y) {
+        return x > y ? x : y;
+    }
+    // looks like it will not compile.  I think this is because we are passing
+    // primitive types in and trying to use them with generics, which is not
+    // allowed
+
+
+    public static void print(List<? extends Number> list) {
+        for (Number n : list)
+            System.out.print(n + " ");
+        System.out.println();
+    }
+}
+
